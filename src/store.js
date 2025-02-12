ff59f4081394b708fdd179b03022d6d187b28297
Henil Malaviya <henilm.dev@gmail.com>
2025-02-12 22:40:33 +0530

    feat: implement store functions

--- src/store.js
+++ src/store.js
@@ -1,2 +1,12 @@
 export const todos = globalThis.todos || [];
 globalThis.todos = todos;
+
+export const addTodo = (todo) => {
+  todos.push(todo);
+  globalThis.todos = todos;
+};
+
+export const removeTodo = (id) => {
+  todos = todos.filter((todo) => todo.id !== id);
+  globalThis.todos = todos;
+};
