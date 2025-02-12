eb70421d72260f31d41e79319833cf4cf3e98b1c
rudraa19 <rudra19206@gmail.com>
2025-02-12 22:25:00 +0530

    implement POST /todos

--- src/routes/create.js
+++ src/routes/create.js
@@ -1,7 +1,22 @@
 import { Hono } from "hono";
-
+import { todos } from "../store"
 export const createRoute = new Hono();
 
-createRoute.post("/", (c) => {
-  return c.text("Hello, World!");
+createRoute.post("/", async (c) => {
+  const { title, desc } = await c.req.json();
+  const id = todos.length + 1;
+  todos.push({
+    id,
+    title,
+    desc
+  })
+  return c.json({
+    error: false,
+    message: "Todos Stored",
+    data: {
+      id
+    },
+
+  });
 });
+
