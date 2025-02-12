e0cca45cd27d7b6dd1745ef95ae1fd3b58715e69
Henil Malaviya <henilm.dev@gmail.com>
2025-02-12 22:19:23 +0530

    feat: implement GET /todos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { createRoute } from "./routes/create";
+import { getRoute } from "./routes/get";
 
 const app = new Hono();
 
 app.route("/todos", createRoute);
+app.route("/todos", getRoute);
 
 export default app;
---
+++ src/routes/get.js
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+import { todos } from "../store";
+
+export const getRoute = new Hono();
+
+getRoute.get("/", (ctx) => {
+  return ctx.json({
+    error: false,
+    message: "Todos Fetched",
+    data: todos,
+  });
+});
