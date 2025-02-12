924ef8b22e612a68d74701637f2e615f1b40d547
rudraa19 <rudra19206@gmail.com>
2025-02-12 22:28:52 +0530

    feat: using UUID

--- src/routes/create.js
+++ src/routes/create.js
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
 import { todos } from "../store"
+import { randomUUID } from "node:crypto"
 export const createRoute = new Hono();
 
 createRoute.post("/", async (c) => {
   const { title, desc } = await c.req.json();
-  const id = todos.length + 1;
+  const id = randomUUID();
   todos.push({
     id,
     title,
